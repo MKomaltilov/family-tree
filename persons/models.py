bc54b726a03b76e7808763febca3929c18df8e81
mko <mko@startext.ru>
2020-05-22 19:37:51 +0700

    loactions added

--- persons/models.py
+++ persons/models.py
@@ -12,19 +12,23 @@ class Person(models.Model):
     patronymic = models.CharField(max_length=300, null=True, blank=True)
     gender = models.CharField(max_length=10, choices=gender_choices)
 
+    birth_location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True, blank=True, related_name='persons_born')
     year_of_birth = models.IntegerField(null=True, blank=True)
     month_of_birth = models.PositiveSmallIntegerField(null=True, blank=True)
     day_of_birth = models.PositiveSmallIntegerField(null=True, blank=True)
 
+    burial_location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True, blank=True, related_name='persons_buried')
     year_of_death = models.IntegerField(null=True, blank=True)
     month_of_death = models.PositiveSmallIntegerField(null=True, blank=True)
     day_of_death = models.PositiveSmallIntegerField(null=True, blank=True)
 
+    living_locations = models.ManyToManyField('Location', null=True, blank=True, related_name='persons_living')
+
     additional_information = models.TextField(null=True, blank=True)
 
-    mother = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='m')
-    father = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='f')
-    spouse = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='s')
+    mother = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='kids_mother')
+    father = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='kids_father')
+    spouse = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='spouse_reverse')
 
     photo = models.ImageField(upload_to="persons/", null=True, blank=True)
 
@@ -32,3 +36,11 @@ class Person(models.Model):
 
     def __str__(self):
         return f"{self.name} {self.surname}"
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
